732e59180684c671acdfd91290a44633838cb35a
hsu rynki <gernischt@gmail.com>
2025-06-22 10:32:28 +0800

    update consts.ts

--- lib/consts.ts
+++ lib/consts.ts
@@ -1,5 +1,5 @@
 
-export const MS_1_MINS = 60 * 1000
-export const MS_5_MINS = 5 * 60 * 1000
-export const MS_1_HOUR = 60 * 60 * 1000
-export const MS_24_HOURS = 24 * 60 * 60 * 1000
+export const MS_1_MIN = 60 * 1000
+export const MS_5_MINS = 5 * MS_1_MIN
+export const MS_1_HOUR = 60 * MS_1_MIN
+export const MS_24_HOURS = 24 * MS_1_HOUR
